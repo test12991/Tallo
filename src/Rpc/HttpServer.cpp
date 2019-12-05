afc234bd978c333162df707ca55e8843b150b5c7
Mika Lindqvist <postmaster@raasu.org>
2019-12-05 11:41:46 +0200

    [Rpc] When connection handling in HttpServer catches an exception, bind the acceptLoop again.

--- src/Rpc/HttpServer.cpp
+++ src/Rpc/HttpServer.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+// Copyright (c) 2019, The Talleo developers
 //
 // This file is part of Bytecoin.
 //
@@ -92,6 +93,7 @@ void HttpServer::acceptLoop() {
   } catch (System::InterruptedException&) {
   } catch (std::exception& e) {
     logger(WARNING) << "Connection error: " << e.what();
+    workingContextGroup.spawn(std::bind(&HttpServer::acceptLoop, this));
   }
 }
 
