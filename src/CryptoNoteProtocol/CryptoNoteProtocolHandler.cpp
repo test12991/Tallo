a3b5c810541f55b1b49dedf4d5e32c3912f31c10
Mika Lindqvist <postmaster@raasu.org>
2023-03-17 16:55:58 +0200

    [Daemon] Reformat synchronization message * Only show how many days daemon is behind or ahead the remote node if difference is at least 24 hours

--- src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
+++ src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
@@ -247,11 +247,14 @@ bool CryptoNoteProtocolHandler::process_payload_sync_data(const CORE_SYNC_DATA& 
     }
   } else {
     int64_t diff = static_cast<int64_t>(hshd.current_height) - static_cast<int64_t>(get_current_blockchain_height());
+    uint64_t blocks = std::abs(diff);
+    uint64_t days = blocks / (24 * 60 * 60 / m_currency.difficultyTarget());
 
     logger(diff >= 0 ? (is_inital ? Logging::INFO : Logging::DEBUGGING) : Logging::TRACE, Logging::BRIGHT_GREEN) << context <<
       "Your Talleo node is syncing with the network. You are "
       // << get_current_blockchain_height() << " -> " << hshd.current_height
-      << std::abs(diff) << " blocks (" << std::abs(diff) / (24 * 60 * 60 / m_currency.difficultyTarget()) << " days) "
+      << blocks << " " << ((blocks == 1) ? "block" : "blocks")
+      << (days > 0 ? " (" + std::to_string(days) + " " + ((days == 1) ? "day" : "days") + ") " : " ")
       << (diff >= 0 ? std::string("behind") : std::string("ahead of")) << " the remote node. Synchronization started! " << std::endl;
 
     logger(Logging::DEBUGGING) << "Remote top block height: " << hshd.current_height << ", id: " << hshd.top_id;
